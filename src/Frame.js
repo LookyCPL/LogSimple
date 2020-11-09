2453ab0e49cec5548c7aa15e9575ef077e23f70d
lcepelak <lukas.cepelak@cleverlance.com>
2020-11-09 17:16:41 +0100

    deleting shits

--- src/Frame.js
+++ src/Frame.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import {useShareableState} from "./States";
-import {useBetween} from 'use-between';
 import "./Frame.css"
 
 
-
-
 export const Frame = (props) => {
-
-    const {frameList} = useBetween(useShareableState);
 
     return (
         <div className={props.class}>
