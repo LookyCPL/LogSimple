a6a716406b461e3eb4a3c22bc06021b0222a24fb
Filip Sakala <filip.sakala@vacuumlabs.com>
2020-11-10 15:45:24 +0100

    Methods refactoring

--- src/Upload.js
+++ src/Upload.js
@@ -1,24 +1,21 @@
 import React from "react";
 import { useBetween } from "use-between";
 import { useShareableState } from "./states";
-import { dateTypeRecognizer, dataSeparateGet, bodyFormatter } from "./methods";
-
+import { dataSeparate, bodyFormatter } from "./methods";
 
 export const Upload = (props) => {
-
   const {
-        setFrameList,
-        frameList,
-        setRowCount,
-        setIsUploaded,
-        setFileName,
-    } = useBetween(useShareableState);
-  
-  const fillArray = (fileContent) => {
-    let tempObject = dateTypeRecognizer(frameList, fileContent);
-    tempObject = dataSeparateGet(tempObject, fileContent, tempObject.key);
+    setFrameList,
+    frameList,
+    setRowCount,
+    setIsUploaded,
+    setFileName,
+  } = useBetween(useShareableState);
 
-    setFrameList(tempObject);
+  const fillArray = (fileContent) => {
+    const separated = dataSeparate(fileContent);
+
+    setFrameList(separated);
     setRowCount(frameList.key.length);
     setIsUploaded(true);
     //setFrameList(bodyFormatter(frameList, frameList.get("data")));
@@ -32,7 +29,7 @@ export const Upload = (props) => {
     reader.onload = (e) => {
       fillArray(e.target.result);
     };
-    
+
     setFileName(e.target.files[0].name);
   };
 
--- src/methods.js
+++ src/methods.js
@@ -1,65 +1,54 @@
-import moment from 'moment'
-import React from 'react';
+import moment from "moment";
+import React from "react";
 
 // --------------------------------------------- UPLOAD LOGIC -------------------------------------------
-export const dateTypeRecognizer = (object, inputText) => {
-
-    const dateFormatType = "YYYY-MM-DD HH:mm:ss,SSS";
-    let keyList = [];
-    let classList = [];
-    let filterItemList = [];
-    let rows = inputText.split('\n');
-    let isDate;
-
-    for (const row of rows) {
-
-        let itemList = [];
-        isDate = moment(row.substring(0, dateFormatType.length), dateFormatType, true).isValid();
-
-        if (isDate) {
-            keyList.push(row.substring(0, dateFormatType.length));
-            classList.push("default");
-            filterItemList.push(itemList);
-        }
-    }
-    object.filterItemList = filterItemList;
-    object.class = classList;
-    object.key = keyList;
-    return {...object};
+const separateDates = (rows) => {
+  const dateFormatType = "YYYY-MM-DD HH:mm:ss,SSS";
+  return rows
+    .map((row) => row.substring(0, dateFormatType.length))
+    .filter((formattedDate) =>
+      moment(formattedDate, dateFormatType, true).isValid()
+    );
 };
 
-export const dataSeparateGet = (object, inputText, separators) => {
+export const dataSeparate = (fileContent) => {
+  const rows = fileContent.split("\n");
+  let keyList = separateDates(rows);
 
-    let dataList = [];
-    let rows = inputText.split('\n');
-    let separatorCount = 0;
-    let rowCount = 0;
+  let dataList = [];
+  let separatorCount = 0;
+  let rowCount = 0;
 
-    for (const row of rows) {
+  rows.forEach((row) => {
+    const keyListOnSeparator = keyList[separatorCount];
+    let index = row.search(keyListOnSeparator);
 
-        let index = row.search(separators[separatorCount]);
-
-        if (index === 0) {
-
-            dataList.push((row.substring(separators[separatorCount].length, row.length)));
-            if (separatorCount < separators.length - 1) {
-                separatorCount++;
-            }
-            rowCount++;
-        } else if (index > 0) {
-
-            dataList.push(row.substring(0, index) + row.substring((index + separators[separatorCount].length), row.length));
-            if (separatorCount < separators.length - 1) {
-                separatorCount++;
-            }
-            rowCount++;
-        } else {
-
-            dataList.splice(rowCount - 1, rowCount - 1, dataList[rowCount - 1] + '\n' + row);
-        }
+    if (index === 0) {
+      dataList.push(row.substring(keyListOnSeparator.length, row.length));
+      if (separatorCount < keyList.length - 1) separatorCount++;
+      rowCount++;
+    } else if (index > 0) {
+      dataList.push(
+        row.substring(0, index) +
+          row.substring(index + keyListOnSeparator.length, row.length)
+      );
+      if (separatorCount < keyList.length - 1) separatorCount++;
+      rowCount++;
+    } else {
+      dataList.splice(
+        rowCount - 1,
+        rowCount - 1,
+        dataList[rowCount - 1] + "\n" + row
+      );
     }
-    object.data = dataList;
-    return {...object};
+  });
+
+  return {
+    key: keyList,
+    data: dataList,
+    class: Array(keyList.length).fill("default"),
+    filterItemList: Array(keyList.length).fill([]),
+  };
 };
 /*
 export const bodyFormatter = (map, dataList) => {
@@ -82,158 +71,95 @@ export const bodyFormatter = (map, dataList) => {
 
 // ------------------------- FILTER LOGIC -----------------------------------------------
 export const filterItemAddHandler = (filterList, filter) => {
+  const hasDuplicity = filterList.some((f) => f.search(filter) > -1);
+  if (hasDuplicity) return "duplicity";
 
-    //duplicity check
-    for (let g = 0; g < filterList.length; g++) {
-
-        if (filterList[g].search(filter) > -1) {
-            return "duplicity";
-        }
-    }
-    let tempList = [];
-
-    //sub-duplicity check
-    for (let g = 0; g < filterList.length; g++) {
-
-        if (filter.search(filterList[g]) === -1) {
-            tempList.push(filterList[g]);
-        }
-    }
-    tempList.push(filter);
-    return tempList;
+  const tempList = filterList.filter((f) => filter.search(f) === -1);
+  tempList.push(filter);
+  return tempList;
 };
 
-
 export const filterItemRemoveHandler = (filterList, filter) => {
-
-    for (let g = 0; g < filterList.length; g++) {
-
-        if (filterList[g] === filter) {
-            filterList.splice(g, 1);
-        }
+  for (let g = 0; g < filterList.length; g++) {
+    if (filterList[g] === filter) {
+      filterList.splice(g, 1);
     }
-    return new Array(...filterList);
+  }
+  return new Array(...filterList);
 };
 
 export const filterItemAssign = (object, filter) => {
+  let dataList = object.data;
+  let tempClassList = [];
 
-    let dataList = object.data;
-    let tempClassList = [];
-
-    for (let r = 0; r < dataList.length; r++) {
-
-        // delete sub-duplicities throw all filter items
-        if (object.filterItemList[r].length > 0) {
-
-            let tempFilterItemList = [];
-
-            for (const tempFilterItem of object.filterItemList[r]) {
-
-                if (filter.search(tempFilterItem.id) === -1) {
-                    tempFilterItemList.push(tempFilterItem);
-                }
-            }
-            object.filterItemList[r] = tempFilterItemList;
-        }
-
-        // check if new filter is match in data
-        if (dataList[r].search(filter) > -1) {
-
-            let matches = dataList[r].matchAll(RegExp(filter, 'g'));
-            let subIndexList = [];
-
-            for (const match of matches) {
-                subIndexList.push({
-                    id: match.index,
-                    start: match.index,
-                    end: match.index + match[0].length
-                });
-            }
-
-            let filterItem = {
-                id: filter,
-                caseSens: false,
-                matchWord: false,
-                indexList: subIndexList
-            };
-
-            object.filterItemList[r].push(filterItem);
-
-            tempClassList.push("default");
-        }
-        else {
-            tempClassList.push("hidden");
-        }
+  for (let r = 0; r < dataList.length; r++) {
+    // delete sub-duplicities throw all filter items
+    if (object.filterItemList[r].length > 0) {
+      object.filterItemList[r] = object.filterItemList[r].filter(
+        (f) => filter.search(f.id) === -1
+      );
     }
-    object.class = tempClassList;
-    return {...object};
+
+    // check if new filter is match in data
+    if (dataList[r].search(filter) > -1) {
+      const matches = dataList[r].matchAll(RegExp(filter, "g"));
+      const subIndexList = Array.from(matches).map((match) => ({
+        id: match.index,
+        start: match.index,
+        end: match.index + match[0].length,
+      }));
+      const filterItem = {
+        id: filter,
+        caseSens: false,
+        matchWord: false,
+        indexList: subIndexList,
+      };
+
+      object.filterItemList[r].push(filterItem);
+
+      tempClassList.push("default");
+    } else {
+      tempClassList.push("hidden");
+    }
+  }
+  object.class = tempClassList;
+  return { ...object };
 };
 
 export const filterItemUnAssignHandler = (object, filter) => {
+  let tempClassList = [];
 
-    let tempClassList = [];
-
-    for (let w = 0; w < object.filterItemList.length; w++) {
-
-        if (object.filterItemList[w].length > 0) {
-
-            for (let i = 0; i < object.filterItemList[w].length; i++) {
-
-                if (object.filterItemList[w][i].id === filter) {
-                    object.filterItemList[w].splice(i, 1);
-                    break;
-                }
-            }
+  for (let w = 0; w < object.filterItemList.length; w++) {
+    if (object.filterItemList[w].length > 0) {
+      for (let i = 0; i < object.filterItemList[w].length; i++) {
+        if (object.filterItemList[w][i].id === filter) {
+          object.filterItemList[w].splice(i, 1);
+          break;
         }
-        if (object.filterItemList[w].length > 0) {
-            tempClassList.push("default");
-        } else {
-            tempClassList.push("hidden");
-        }
+      }
     }
+    if (object.filterItemList[w].length > 0) {
+      tempClassList.push("default");
+    } else {
+      tempClassList.push("hidden");
+    }
+  }
 
-    if (object.filterItemList.flat().length === 0) {
-        tempClassList.fill("default",0, tempClassList.length);
-    }
-    object.class = tempClassList;
-    return {...object};
+  if (object.filterItemList.flat().length === 0) {
+    tempClassList.fill("default", 0, tempClassList.length);
+  }
+  object.class = tempClassList;
+  return { ...object };
 };
-
 
 // ------------------------- USAGE LOGIC -----------------------------------------------
 
-
-export const filterIndexListMerge = (filterItemList) => {
-
-    let tempIndexList = [];
-
-    for (const filterItem of filterItemList) {
-
-        tempIndexList = [...tempIndexList, ...filterItem.indexList];
-    }
-
-    let swapped = true;
-    let counter = 0;
-    let tempValue;
-
-    while (swapped) {
-        swapped = false;
-        counter++;
-        for (let i = 0; i < tempIndexList.length - counter; i++) {
-            if (tempIndexList[i].id > tempIndexList[i + 1].id) {
-                tempValue = tempIndexList[i];
-                tempIndexList[i] = tempIndexList[i + 1];
-                tempIndexList[i + 1] = tempValue;
-                swapped = true;
-            }
-        }
-    }
-    return tempIndexList;
-};
-
-
-const generateTag = () => {
-    return (
-        <b>Hello</b>
-    )
-};
+export const filterIndexListMerge = (filterItemList) =>
+  filterItemList
+    .map((filterItem) => filterItem.indexList)
+    .flat()
+    .sort((a, b) => {
+      if (a.id > b.id) return 1;
+      if (a.id < b.id) return -1;
+      return 0;
+    });
